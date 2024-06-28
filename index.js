3cd28508bd32a316f980a1f22843faa1ca191c2c
AksBad007 <arora.akshat007@gmail.com>
2024-06-29 05:05:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const contentfulExport = require("contentful-export");
+const port = 3000;
+
+app.post("/", async (req, res) => {
+  const { spaceId, managementToken } = req.body;
+  let result;
+
+  await contentfulExport({ spaceId, managementToken })
+    .then((res) => {
+      console.log("Your space data:", res);
+      result = res;
+    })
+    .catch((err) => {
+      console.error("Oh no! Some errors occurred!", err);
+      result = err;
+    });
+
+  res.send(result);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
